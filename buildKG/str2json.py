402430689ed5ed0e13e88c67c577d8961fd8395e
徐明威 <3178665171@qq.com>
2023-12-14 15:41:11 +0800

    main

--- buildKG/str2json.py
+++ buildKG/str2json.py
@@ -1,12 +1,13 @@
 import json
+import re
 def str2json(raw_string):
     # 解码为字符串
     decoded_string = raw_string.encode('utf-8').decode('utf-8')
 
     # 去除字符串内部的"\\"和"\\n"
     cleaned_string = decoded_string.replace("\\", "").replace("\\n", "").replace("```","").replace("json","")
-
-
+    nodes_array=[]
+    edges_array=[]
     # 尝试解析为JSON
     try:
         json_data = json.loads(cleaned_string)
@@ -15,5 +16,15 @@ def str2json(raw_string):
         # print(json.dumps(json_data, indent=2, ensure_ascii=False))  # 打印格式化后的JSON
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
-    
+        pattern = r'```(.*?)```'
+        cleaned_string = re.findall(pattern, raw_string, re.DOTALL)
+        print(cleaned_string)
+        if len(cleaned_string)==1:
+            json_data = json.loads(cleaned_string[0])
+            nodes_array=json_data["point"]
+            edges_array=json_data["edge"]
+        else:
+            nodes_array=[]
+            edges_array=[]
+
     return nodes_array,edges_array
